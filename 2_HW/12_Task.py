aba2c4c0cab658277e48a5a67692e8d09520e2cb
belka.boo <ibelka.boo@gmail.com>
2023-03-16 00:15:18 +0300

    12_Task add solution

--- 2_HW/12_Task.py
+++ 2_HW/12_Task.py
@@ -2,3 +2,21 @@
 # Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. 
 # Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
 # Помогите Кате отгадать задуманные Петей числа.
+
+from random import randint
+
+x = randint(0,1000)
+y = randint(0,1000)
+sum, prod = x + y , x * y
+
+print('Петя загадал 2 числа, сумма этих чисел - {} , произведение - {}\n'.format(sum,prod))
+
+flag = False
+for i in range(1001):
+    for j in range(1001):
+        if i + j == sum and i * j == prod:
+            print('{} и {}'.format(i,j))
+            flag = True           
+            break
+    if flag:
+        break
