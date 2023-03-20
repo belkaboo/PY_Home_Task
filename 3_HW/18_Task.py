8c01a6a879b277ad8c5b2b727d613bc38830f074
belka.boo <ibelka.boo@gmail.com>
2023-03-21 01:45:51 +0300

    18_Task: add solution

--- 3_HW/18_Task.py
+++ 3_HW/18_Task.py
@@ -8,3 +8,18 @@
 #     1 2 3 4 5
 #     6
 #     -> 5
+
+from random import randint
+
+num = int(input('Введите кол-во элементов массива: '))
+x = int(input('Введите число X: '))
+
+list_nums = [randint(1,50) for i in range(num)]
+res_index, diff = 0, abs(list_nums[0] - x)
+
+for i in range(1, num):
+    if abs(list_nums[i] - x) < diff:
+        diff, res_index = abs(list_nums[i] - x), i
+        
+print(list_nums)
+print('{} - ближайший элемент в массиве к числу {}' .format(list_nums[res_index],x))
