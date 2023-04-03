8a00213a2ad73d7997f192a23cc7efcdedddc699
belka.boo <ibelka.boo@gmail.com>
2023-04-04 02:43:16 +0300

    32_Task add solution

--- 6_HW/32_Task.py
+++ 6_HW/32_Task.py
@@ -1,4 +1,15 @@
 # Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону 
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
 
-    
+from random import randint
+
+n_min = int(input('Введите нижнюю границу отсева: '))
+n_max =int(input('Введите верхнюю границу отсева: '))
+
+list_1 = [randint(-10,10) for i in range(5,10)]
+print(type(list_1))
+print(list_1)
+
+for i in range(list_1.__len__()):
+    if n_min <= list_1[i] <= n_max:
+        print(i, sep:=' ', end= '')
